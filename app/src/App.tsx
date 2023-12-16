40603a67066cad5fbd574fee1dc68326ae9b43ea
Marnix <marnix@valkdigital.nl>
2023-12-16 13:01:38 +0100

    Add WebConfig for CORS support and fetch todos in App component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,27 @@
+import React, { useEffect, useState } from 'react';
+
+interface Todo {
+  id: number;
+  title: string;
+}
+
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/todos')
+      .then(response => response.json())
+      .then(data => setTodos(data));
+  }, []);
+
   return (
     <div className="App">
-
       <h1>Hello, React!</h1>
-      
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }

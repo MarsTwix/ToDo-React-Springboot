c75c1bf16c290413f5cae598ad50a2e2990a216c
Marnix <marnix@valkdigital.nl>
2023-12-16 10:59:19 +0100

    Update table and column names in db/Dockerfile and db/init.sql

--- db/init.sql
+++ db/init.sql
@@ -1,15 +1,14 @@
 -- Create a table to store to-do items
-CREATE TABLE todo_items (
+CREATE TABLE todos (
   id SERIAL PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
   description TEXT,
   completed BOOLEAN NOT NULL DEFAULT FALSE,
-  due_date DATE,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Insert some sample to-do items
-INSERT INTO todo_items (title, description, due_date) VALUES
-  ('Finish coding project', 'Complete all required tasks and submit project', '2023-11-25'),
-  ('Buy groceries', 'Milk, eggs, bread, fruits, vegetables', '2023-11-20'),
-  ('Go to the gym', '30 minutes of cardio and strength training', '2023-11-22');
+INSERT INTO todos (title, description, due_date) VALUES
+  ('Finish coding project', 'Complete all required tasks and submit project'),
+  ('Buy groceries', 'Milk, eggs, bread, fruits, vegetables'),
+  ('Go to the gym', '30 minutes of cardio and strength training');

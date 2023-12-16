17a1d5348bc8334f1af0034828db5741d44fc504
Marnix <marnix@valkdigital.nl>
2023-12-16 11:48:38 +0100

    Update Dockerfile and Vite configuration

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { host: "localhost", port: 3000 },
-  preview: { host: "localhost", port: 3000 },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 3000, // you can replace this port with any port
+  }
 })
